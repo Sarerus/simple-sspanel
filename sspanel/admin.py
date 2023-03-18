24111aa14aaf7bfa5e9499d92ab9af5cc6a59fcf
k8s <k8s@dosoos.com>
2023-03-18 16:32:38 +0800

    admin add v2ray account

--- sspanel/admin.py
+++ sspanel/admin.py
@@ -1,3 +1,52 @@
 from django.contrib import admin
+from django.utils.translation import gettext as _
+from django.contrib.auth.models import User
+from sspanel.models import Account
+
 
 # Register your models here.
+@admin.register(Account)
+class AccountAdmin(admin.ModelAdmin):
+    list_display = [
+        'user_username',
+        'uuid',
+        'level',
+        'remark',
+        'create_time',
+    ]
+    fieldsets = [
+        (None, {
+            'fields': [
+                'username',
+                'password',
+                'uuid',
+                'level',
+            ]
+        }
+        ),
+        (_('General Information'),
+            {
+                'fields': [
+                    'remark',
+                    'status',
+                    'enable',
+                ]
+        }
+        ),
+    ]
+
+    @admin.display(ordering='user__username', description=_('Username'))
+    def user_username(self, obj):
+        return obj.user.username
+
+    def get_queryset(self, request):
+        return super(AccountAdmin, self).get_queryset(request).select_related('user')
+
+    def save_model(self, request, obj, form, change):
+        if not change:
+            user = User.objects.create_user(
+                username=obj.username, 
+                password=obj.password
+            )
+            obj.user = user
+        super().save_model(request, obj, form, change)
--- sspanel/models.py
+++ sspanel/models.py
@@ -3,19 +3,35 @@ from django.utils.translation import gettext as _
 from django.contrib.auth.models import User
 from core.models import Base
 
+import uuid
+
+
 # Create your models here.
 class Account(Base):
 
     # foreign system user
-    account = models.OneToOneField(
+    user = models.OneToOneField(
         User, 
         on_delete=models.CASCADE, 
         verbose_name=_('System Account'))
+
+    # TODO temp alias to user.username
+    username = models.CharField(
+        _('temp username'), 
+        null=True,
+        max_length=128)
+
+    # TODO temp alias to user.password
+    password = models.CharField(
+        _('temp password'), 
+        null=True,
+        max_length=128)
     
     # uuid for v2ray client
     uuid = models.UUIDField(
         'UUID',
         unique=True,
+        default=uuid.uuid4,
         help_text=_('V2ray-core client uuid'))
 
     # user level for v2ray-core client field

38362289fbccef1912768e9b3c005f9a89842168
k8s <k8s@dosoos.com>
2023-03-18 22:37:58 +0800

    remove v2ray hook on admin code

--- sspanel/admin.py
+++ sspanel/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from django.utils.translation import gettext as _
-from django.contrib.auth.models import User
 from sspanel.models import Account
 
 
@@ -41,12 +40,3 @@ class AccountAdmin(admin.ModelAdmin):
 
     def get_queryset(self, request):
         return super(AccountAdmin, self).get_queryset(request).select_related('user')
-
-    def save_model(self, request, obj, form, change):
-        if not change:
-            user = User.objects.create_user(
-                username=obj.username, 
-                password=obj.password
-            )
-            obj.user = user
-        super().save_model(request, obj, form, change)

1a1ae2e3e75674ec5d240e7a398180d6d673419a
k8s <k8s@dosoos.com>
2023-03-18 22:38:21 +0800

    hook user info change to async v2ray

--- sspanel/models.py
+++ sspanel/models.py
@@ -1,9 +1,15 @@
 from django.db import models
 from django.utils.translation import gettext as _
 from django.contrib.auth.models import User
+from backend.tasks import v2ray_add_user, v2ray_del_user
 from core.models import Base
 
 import uuid
+
+import logging
+
+# Get an instance of a logger
+logger = logging.getLogger(__name__)
 
 
 # Create your models here.
@@ -38,3 +44,26 @@ class Account(Base):
     level = models.PositiveIntegerField(
         _('User Level'), 
         default=0)
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            # create system user
+            user = User.objects.create_user(
+                username=self.username, 
+                password=self.password
+            )
+            self.user = user 
+            # add v2ray user
+            v2ray_add_user.delay(self.uuid)
+        else:
+            # if change account uuid will update v2ray user
+            before = Account.objects.get(pk=self.id)
+            if self.uuid != before.uuid:
+                v2ray_del_user.delay(before.uuid)
+                v2ray_add_user.delay(self.uuid)
+        return super(Account, self).save(*args, **kwargs)
+
+    def delete(self, *args, **kwargs):
+        # delete v2ray user
+        v2ray_del_user.delay(self.uuid)
+        return super(Account, self).delete(*args, **kwargs)

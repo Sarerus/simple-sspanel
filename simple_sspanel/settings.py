69faf9fafbb893ca25094bb0e22838c6762e543d
k8s <k8s@dosoos.com>
2023-03-20 12:33:13 +0800

    add static folder to settings

--- simple_sspanel/settings.py
+++ simple_sspanel/settings.py
@@ -131,6 +131,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT =  BASE_DIR / "staticfiles"
+STATICFILES_DIRS = [
+    STATIC_ROOT,
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
